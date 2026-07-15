e37df78f3de3584fbeb56e1f298b9e7580703725
zzl <512929087@qq.com>
2026-07-15 21:56:32 +0800

    feat: add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css'
 import bgImage from './assets/bg.jpg'
 
 const LOCBOX_SCHEME = 'locbox://'
 const IOS_DOWNLOAD_URL = 'https://apps.apple.com/vn/app/locbox/idXXXXXXXX'
-const ANDROID_DOWNLOAD_URL = 'https://download.locbox.vn/locbox-latest.apk'
+const ANDROID_FALLBACK_URL = 'https://download.locbox.vn/locbox-latest.apk'
 
 /** 尝试唤起 App，若未安装则跳转下载页 */
 function openApp(downloadUrl: string) {
@@ -30,6 +32,25 @@ function openApp(downloadUrl: string) {
 }
 
 function App() {
+  const [androidUrl, setAndroidUrl] = useState(ANDROID_FALLBACK_URL)
+
+  useEffect(() => {
+    axios
+      .post<{ data: { url: string } }>(
+        'https://test.locbox.vn/api/system/GetAppVersion',
+        { app_name: 'android' },
+        { headers: { token: 'A20260330135549381179xdso' } }
+      )
+      .then(({ data }) => {
+        if (data?.data?.url) {
+          setAndroidUrl(data.data.url)
+        }
+      })
+      .catch(() => {
+        // 接口失败时使用兜底链接
+      })
+  }, [])
+
   return (
     <div
       className="app-container"
@@ -44,7 +65,7 @@ function App() {
         </a>
         <a
           className="download-btn"
-          onClick={() => openApp(ANDROID_DOWNLOAD_URL)}
+          onClick={() => openApp(androidUrl)}
         >
           {/* Android 下载 */}
         </a>
