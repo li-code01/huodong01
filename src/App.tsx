8b753376c466fc6733738774fa9b97a5407f5ca8
zzl <512929087@qq.com>
2026-07-15 22:32:02 +0800

    feat: app

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import './App.css'
 import bgImage from './assets/bg.jpg'
 
 const LOCBOX_SCHEME = 'locbox://'
-const IOS_DOWNLOAD_URL = 'https://apps.apple.com/vn/app/locbox/idXXXXXXXX'
+const IOS_FALLBACK_URL = 'https://apps.apple.com/vn/app/locbox/idXXXXXXXX'
 const ANDROID_FALLBACK_URL = 'https://download.locbox.vn/locbox-latest.apk'
+const API_URL = 'https://locbox.vn/api/system/GetAppVersion'
+const API_TOKEN = 'A20260710150725959755piyc'
 
 /** 尝试唤起 App，若未安装则跳转下载页 */
 function openApp(downloadUrl: string) {
@@ -33,18 +35,22 @@ function openApp(downloadUrl: string) {
 
 function App() {
   const [androidUrl, setAndroidUrl] = useState(ANDROID_FALLBACK_URL)
+  const [iosUrl, setIosUrl] = useState(IOS_FALLBACK_URL)
 
   useEffect(() => {
-    axios
-      .post<{ data: { url: string } }>(
-        'https://locbox.vn/api/system/GetAppVersion',
-        { app_name: 'android' },
-        { headers: { token: 'A20260710150725959755piyc' } }
-      )
-      .then(({ data }) => {
-        if (data?.data?.url) {
-          setAndroidUrl(data.data.url)
-        }
+    const fetchUrl = (appName: string) =>
+      axios
+        .post<{ data: { url: string } }>(
+          API_URL,
+          { app_name: appName },
+          { headers: { token: API_TOKEN } }
+        )
+        .then(({ data }) => data?.data?.url)
+
+    Promise.all([fetchUrl('ios'), fetchUrl('android')])
+      .then(([ios, android]) => {
+        if (ios) setIosUrl(ios)
+        if (android) setAndroidUrl(android)
       })
       .catch(() => {
         // 接口失败时使用兜底链接
@@ -59,7 +65,7 @@ function App() {
       <div className="btn-group">
         <a
           className="download-btn"
-          onClick={() => openApp(IOS_DOWNLOAD_URL)}
+          onClick={() => openApp(iosUrl)}
         >
           {/* iOS 下载 */}
         </a>
